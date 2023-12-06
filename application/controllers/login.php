9244f85f04808392f024896b6a265fbb58239854
j-flatres <134286860+j-flatres@users.noreply.github.com>
2023-12-06 14:50:25 +0100

    connexion + deconnexion

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -11,15 +11,37 @@ class Login extends CI_Controller {
     }
 
     public function index() {
-        // Afficher le formulaire de connexion
-        $this->load->view('login/login_form');
+        if ($this->session->userdata('user_id')) {
+            // L'utilisateur est connecté, afficher un message de bienvenue
+            $data['welcome_message'] = 'Bienvenue, ' . $this->session->userdata('username') . ' !';
+            $this->load->view('login/welcome_view', $data);
+        } else {
+            // L'utilisateur n'est pas connecté, afficher le formulaire de connexion
+            $this->load->view('login/login_form');
+        }
+    } 
+
+    public function welcome_view() {
+        // Vérifiez si l'utilisateur est connecté
+        if ($this->session->userdata('user_id')) {
+            // Récupérez les informations nécessaires depuis la session
+            $data['username'] = $this->session->userdata('username'); // Assurez-vous d'avoir 'username' dans votre session
+    
+            // Chargez la vue welcome_view avec les données
+            $this->load->view('login/welcome_view', $data);
+        } else {
+            // Si l'utilisateur n'est pas connecté, redirigez-le vers la page de connexion
+            redirect('login');
+        }
     }
 
+    
+    
     public function process_login() {
         // Règles de validation pour le formulaire de connexion
         $this->form_validation->set_rules('login', 'Login', 'required');
         $this->form_validation->set_rules('password', 'Password', 'required');
-
+    
         if ($this->form_validation->run() == FALSE) {
             // Si la validation échoue, réafficher le formulaire de connexion
             $this->load->view('login/login_form');
@@ -27,15 +49,15 @@ class Login extends CI_Controller {
             // Si la validation réussit, vérifier les informations d'identification
             $login = $this->input->post('login');
             $password = md5($this->input->post('password')); // Utilisez des méthodes de hachage sécurisées en production
-
+    
             $user = $this->User_model->check_login($login, $password);
-
+    
             if ($user) {
                 // Informations d'identification correctes, connectez l'utilisateur
-                $this->session->set_userdata('user_id', $user->id);
+                $this->session->set_userdata('user_id', $user->id);$this->session->set_userdata('username', $user->nom);
                 // Vous pouvez ajouter d'autres données utilisateur à la session si nécessaire
-
-                redirect('accueil'); // Rediriger vers la page d'accueil ou le tableau de bord
+    
+                redirect('login/welcome_view'); // Rediriger vers la page de bienvenue
             } else {
                 // Informations d'identification incorrectes, réafficher le formulaire de connexion avec un message d'erreur
                 $data['error'] = 'Login ou mot de passe incorrect.';
