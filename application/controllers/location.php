c2cd32b72a014e551c51a6710170664fbbc64ecf
owenlm22 <134289178+owenlm22@users.noreply.github.com>
2023-12-16 15:35:56 +0100

    boutique + description

---
+++ application/controllers/location.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Location_model'); // Charger le modèle Product_model
+    }
+
+    public function reserver() {
+        // Récupérer les données envoyées par la requête AJAX
+        $produitId = $this->input->post('produit_id');
+        $utilisateurId = $this->input->post('utilisateur_id');
+        $prixTotal = $this->input->post('prix_total');
+
+        // Chargez le modèle 'Location_model' pour effectuer des opérations de base de données
+        $this->load->model('Location_model');
+
+        // Insérer ces données dans la table "location" via votre modèle
+        $this->Location_model->ajouterLocation($produitId, $utilisateurId, $prixTotal);
+        
+        // Envoyer une réponse (peut être utilisée pour confirmer l'insertion)
+        echo "Vous avez réservé votre article avec succès !";
+    }
+
+}
+?>
--- application/views/product/description.php
+++ application/views/product/description.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <title>Détails du Produit</title>
     <!-- Styles CSS, liens vers les fichiers CSS -->
+    <link rel="icon" type="image/png" href="<?php echo base_url('img/Rectangle 367.png'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/StyleDescrition.css'); ?>">
 </head>
 <body>
+<?php $this->load->view('header');
+    ?>
     <h1>Détails du Produit</h1>
     <div>
         <h2><?php echo $produit['type']; ?></h2>
