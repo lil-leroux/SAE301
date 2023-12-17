cabc1ced5cebb5498e6d0f88c492e9f589e1c3a1
owenlm22 <134289178+owenlm22@users.noreply.github.com>
2023-12-17 16:05:28 +0100

    date ajout

--- application/views/product/description.php
+++ application/views/product/description.php
@@ -7,6 +7,11 @@
     <meta charset="UTF-8">
     <link rel="icon" type="image/png" href="<?php echo base_url('img/Rectangle 367.png'); ?>" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css'); ?>">
+    <!-- date pop up  -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     <title>Description du Produit</title>
 </head>
 
@@ -37,6 +42,8 @@
         <p><strong>Modèle:</strong> <?php echo $produit['modele']; ?></p>
         <p><strong>Prix de Location:</strong> <?php echo $produit['prix_location']; ?> €</p>
         <p><strong>État:</strong> <?php echo $produit['etat']; ?></p>
+        <input type="text" name="daterange" value="" />
+        
         <!-- Affichage d'autres détails du produit selon les besoins -->
     </div>
         <?php
@@ -51,6 +58,15 @@
     // Chargez la barre de navigation
     $this->load->view('footer');
     ?>
+    <script>
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+</script>
 </body>
 
 </html>
